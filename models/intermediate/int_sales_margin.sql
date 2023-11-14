d4ff6564c6c6908ff32ed8f1f76859f5372f8a60
pierre.lewagon@gmail.com <pierre.lewagon@gmail.com>
2023-11-14 18:15:37 +0000

    finance _day ok schema ok

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -2,7 +2,8 @@
 SELECT
     orders_id,
     date_date,
-    revenue,quantity,
+    revenue,
+    quantity,
     round((p.purchase_price*s.quantity),2) as purchase_cost,
     round (s.revenue - (p.purchase_price*s.quantity),2) as margin
 FROM {{ ref("stg_raw__sales") }} s
